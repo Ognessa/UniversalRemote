fae7e744376581f82bf265337e9beb672a70b59e
Viktoria Palykhata <vikapalihata@gmail.com>
2026-04-10 20:25:27 +0200

    feat: added flag to disable logger in runtime

--- core/src/iosMain/kotlin/com/patorika/core/util/LoggerUtil.ios.kt
+++ core/src/iosMain/kotlin/com/patorika/core/util/LoggerUtil.ios.kt
@@ -5,15 +5,17 @@ import kotlin.experimental.ExperimentalNativeApi
 import kotlin.native.Platform
 
 actual object LoggerUtil {
+    actual var isEnabled = true
+
     @OptIn(ExperimentalNativeApi::class)
-    private val loggerAllowed = Platform.isDebugBinary
+    private fun loggerAllowed(): Boolean = Platform.isDebugBinary && isEnabled
 
     actual fun e(
         tag: String,
         message: String,
         throwable: Throwable?,
     ) {
-        if (loggerAllowed) {
+        if (loggerAllowed()) {
             if (throwable != null) {
                 NSLog("ERROR: [$tag] $message. Throwable: $throwable CAUSE ${throwable.cause}")
             } else {
@@ -26,7 +28,7 @@ actual object LoggerUtil {
         tag: String,
         message: String,
     ) {
-        if (loggerAllowed) {
+        if (loggerAllowed()) {
             NSLog("DEBUG: [$tag] $message")
         }
     }
@@ -35,7 +37,7 @@ actual object LoggerUtil {
         tag: String,
         message: String,
     ) {
-        if (loggerAllowed) {
+        if (loggerAllowed()) {
             NSLog("INFO: [$tag] $message")
         }
     }
